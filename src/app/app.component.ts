e2567ea74f1ae22f25823efe6c7b5353d79f3d5f
Solarev Ruslan <ruslan251207@gmail.com>
2023-01-19 19:56:15 +1000

    Finish project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable  } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'AngularAPI';
+export class AppComponent{
+  userName: string = '';
+  response: any;
+  constructor(private http: HttpClient) {}
+  search() {
+    this.http.get('https://api.github.com/users/' + this.userName).subscribe((response) => {
+      this.response = response;
+      console.log(this.response);
+    })
+  }
 }
+
